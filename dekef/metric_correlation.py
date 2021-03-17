8faf467c4a347f15a33efffb07e73e35daff09ef
zhoucx1119 <zhoucx1989@gmail.com>
2021-03-16 20:15:26 -0400

    Update code documentation

--- dekef/metric_correlation.py
+++ dekef/metric_correlation.py
@@ -1,14 +1,15 @@
 import numpy as np
-from denest_kernelexpofam.unnormalized_density import *
+from dekef.unnormalized_density import *
+from check import *
 
 
 def metric_corr(data, test_data, kernel_function, base_density, coef, true_density):
     
     """
-    Computes the value of the cosine angle and correlation between the estimated density values at test_data and
+    Computes the cosine value of the angle and the Pearson's correlation between the density estimates and
     the true density values at test_data as a way of measuring the quality of the estimated density function.
     Let p_0 be the true density function and hat{p} be the estimated density function.
-    The value of the cosine angle is computed as
+    The cosine value of the angle is computed as
       E (p_0 (X) hat{p} (X)) / sqrt{E (p_0 ^ 2 (X)) E (hat{p} ^ 2 (X))},
     where the expectations is estimated using test_data.
     The correlation is the sample Pearson's correlation coefficient.
@@ -24,27 +25,30 @@ def metric_corr(data, test_data, kernel_function, base_density, coef, true_densi
     
     kernel_function : kernel_function object
         The kernel function used to estimate the probability density function.
-        Must be instantiated from the classes with __type__ being 'kernel_function'.
+        __type__ must be 'kernel_function'.
         
     base_density : base_density object
         The base density function used to estimate the probability density function.
-        Must be instantiated from the classes with __type__ being 'base_density'.
+        __type__ must be 'base_density'.
     
     coef : numpy.ndarray
-        The array of coefficients for basis functions in the natural parameter in the estimated density function.
+        The array of coefficients of basis functions in the natural parameter in the estimated density function.
     
     true_density : true_density object
         The true density function from which data are drawn.
-        Must instantiated from the classes in true_density.
     
     Returns
     -------
     float, float
-        The floating point numbers of the cosine angle and correlation
-        between the estimated density values at test_data and the true density values at test_data.
+        The floating point numbers of the cosine value of the angle and the correlation
+        between the density estimates and the true density values at test_data.
 
     """
-    
+
+    check_kernelfunction(kernel_function)
+    check_basedensity(base_density)
+    check_samedata(kernel_function, base_density)
+
     if len(data.shape) == 1:
         data = data.reshape(-1, 1)
     
