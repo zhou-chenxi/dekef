d16b920089a6d071e039d2d70e80133077e9c101
zhoucx1119 <zhoucx1989@gmail.com>
2021-12-21 19:26:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
 	name='dekef',
-	version='2.0',
+	version='3.0',
 	packages=['dekef'],
 	url='https://github.com/zhoucx1119/dekef',
-	license='',
+	license='MIT',
 	author='Chenxi Zhou',
 	author_email='zhoucx1989@gmail.com',
 	description='Density estimation in kernel exponential families.',
