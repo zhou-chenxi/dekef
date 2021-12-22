29343af222cf90d01149183567f9e0c1fe1499d6
zhoucx1119 <zhoucx1989@gmail.com>
2021-12-21 19:25:26 -0500

    Update check.py

--- dekef/check.py
+++ dekef/check.py
@@ -1,7 +1,3 @@
-from dekef.base_density import *
-from dekef.kernel_function import *
-
-
 def check_basedensity(base_density):
 	
 	"""
@@ -44,31 +40,3 @@ def check_kernelfunction(kernel_function):
 	else:
 		
 		pass
-	
-
-# def check_samedata(kernel_function, base_density):
-#
-# 	"""
-# 	Check whether the data in kernel_function and the data in base_density are identical.
-#
-# 	kernel_function : kernel_function object
-# 		The kernel function used to estimate the probability density function.
-# 		__type__ must be 'kernel_function'.
-#
-# 	base_density : base_density object
-# 		The base density function used to estimate the probability density function.
-# 		__type__ must be 'base_density'.
-#
-# 	"""
-#
-# 	kf_data = kernel_function.data
-# 	bd_data = base_density.data
-#
-# 	if not np.allclose(kf_data, bd_data):
-#
-# 		raise ValueError(('The data from kernel_function and base_density are different. '
-# 						  'Please double check the data input.'))
-#
-# 	else:
-#
-# 		pass
