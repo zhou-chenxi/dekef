e7817c25007a8edfa573b7e81fe196122ff49d5c
zhoucx1119 <zhoucx1989@gmail.com>
2021-12-21 19:25:52 -0500

    Update plot_natparam_1d.py

--- dekef/plot_natparam_1d.py
+++ dekef/plot_natparam_1d.py
@@ -5,6 +5,7 @@ from dekef.check import *
 
 def plot_natparam_1d_params(x_limit, y_limit, plot_pts_cnt=2000, figsize=(10, 10),
                             natparam_color='tab:blue', fontsize=20):
+    
     """
     Specifies and returns the plotting parameters used in the function plot_natparam_1d.
 
@@ -38,6 +39,18 @@ def plot_natparam_1d_params(x_limit, y_limit, plot_pts_cnt=2000, figsize=(10, 10
         A dict containing all the plotting parameter inputs.
 
     """
+
+    if len(x_limit) != 2:
+        raise ValueError("The length of x_limit must be 2.")
+
+    if len('y_limit') != 2:
+        raise ValueError("The length of y_limit must be 2.")
+
+    if np.inf in x_limit or -np.inf in x_limit:
+        raise ValueError("x_limit contains non-finite values.")
+
+    if np.inf in y_limit or -np.inf in y_limit:
+        raise ValueError("y_limit in plot_kwargs contains non-finite values.")
     
     output = {'x_limit': x_limit,
               'y_limit': y_limit,
@@ -49,8 +62,8 @@ def plot_natparam_1d_params(x_limit, y_limit, plot_pts_cnt=2000, figsize=(10, 10
     return output
 
 
-def plot_natparam_1d(data, kernel_function, base_density, coef, method, x_label,
-                     plot_kwargs, save_plot=False, save_dir=None, save_filename=None):
+def plot_natparam_1d(data, kernel_function, base_density, basis_type, coef, method, x_label,
+                     plot_kwargs, grid_points=None, save_plot=False, save_dir=None, save_filename=None):
     
     """
     Plots the natural parameter over a bounded interval.
@@ -68,6 +81,15 @@ def plot_natparam_1d(data, kernel_function, base_density, coef, method, x_label,
         The base density function used to estimate the probability density function.
         __type__ must be 'base_density'.
 
+    basis_type : str
+        The type of the basis functions in the natural parameter. Must be one of
+            - 'gubasis', the basis functions being the kernel functions centered at data,
+            - 'smbasis', the basis functions being the same as those in the score matching density estimator, i.e.,
+                         a linear combination of the first two partial derivatives of the kernel functions centered
+                         at data,
+            - 'grid_points', the basis functions being the kernel functions centered at a set of
+                             pre-specified grid points.
+    
     coef : numpy.ndarray
         The array of coefficients of basis functions in the estimated density function.
 
@@ -79,7 +101,11 @@ def plot_natparam_1d(data, kernel_function, base_density, coef, method, x_label,
 
     plot_kwargs : dict
         The dict containing plotting parameters returned from the function plot_natparam_1d_params.
-
+        
+    grid_points : numpy.ndarray, optional
+        The set of grid points at which the kernel functions are centered.
+        Only need to supple when basis_type is 'grid_points'. Default is None.
+        
     save_plot : bool, optional
         Whether to save the plot of the density estimate as a local file; default is False.
 
@@ -98,53 +124,52 @@ def plot_natparam_1d(data, kernel_function, base_density, coef, method, x_label,
         natparam_vals, the values of the vertical axis for plotting.
         
     """
+
+    check_kernelfunction(kernel_function)
+    check_basedensity(base_density)
     
-    if len(data.shape) != 1:
+    if len(data.shape) == 1:
         data = data.reshape(-1, 1)
     
     if data.shape[1] != 1:
-        raise ValueError("The data should be of 1 column.")
+        raise ValueError("The data should be 1-dimensional.")
+    
+    if len(grid_points.shape) == 1:
+        grid_points = grid_points.reshape(-1, 1)
         
-    if len(plot_kwargs['x_limit']) != 2:
-        raise ValueError("The length of x_limit in plot_kwargs must be 2.")
-    
-    if len(plot_kwargs['y_limit']) != 2:
-        raise ValueError("The length of y_limit in plot_kwargs must be 2.")
-    
-    if np.inf in plot_kwargs['x_limit'] or -np.inf in plot_kwargs['x_limit']:
-        raise ValueError("The 'x_limit' in plot_kwargs contains non-finite values.")
-    
-    if np.inf in plot_kwargs['y_limit'] or -np.inf in plot_kwargs['y_limit']:
-        raise ValueError("The 'y_limit' in plot_kwargs contains non-finite values.")
-    
     coef = coef.reshape(-1, 1)
-    n_obs = data.shape[0]
     plot_domain = [[plot_kwargs['x_limit'][0], plot_kwargs['x_limit'][1]]]
     plot_pts_cnt = plot_kwargs['plot_pts_cnt']
     
     # prepare the newx for plotting
     x0_cand = np.linspace(plot_domain[0][0], plot_domain[0][1], num=plot_pts_cnt).reshape(-1, 1)
 
-    if coef.shape[0] == n_obs:
+    unnorm = UnnormalizedDensity(
+        data=data,
+        kernel_function=kernel_function,
+        base_density=base_density,
+        coef=coef,
+        basis_type=basis_type,
+        grid_points=grid_points
+    )
     
+    if basis_type == 'gubasis':
+        
         # Gu's basis
         plot_val = np.matmul(kernel_function.kernel_gram_matrix(x0_cand).T, coef).flatten()
     
-    elif coef.shape[0] == n_obs + 1:
+    elif basis_type == 'smbasis':
         
         # score matching basis
-        unnorm = UnnormalizedDensity(
-            data=data,
-            kernel_function=kernel_function,
-            base_density=base_density,
-            coef=coef)
-        
         plot_val = unnorm.natural_param_eval_smbasis(x0_cand)
     
+    elif basis_type == 'grid_points':
+    
+        plot_val = np.matmul(kernel_function.kernel_gram_matrix(x0_cand).T, coef).flatten()
+        
     else:
     
-        raise ValueError(("The length of coef is not correct and matches neither Gu's basis functions "
-                          "nor score matching basis functions."))
+        raise ValueError(f"basis_type must be one of 'gubasis', 'smbasis', and 'grid_points', but got {basis_type}.")
     
     fig = plt.figure(figsize=plot_kwargs['figsize'])
     left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
