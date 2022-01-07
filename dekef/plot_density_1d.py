7c98ba644bff3521361665b254be677cdcafe2e8
zhoucx1119 <zhoucx1989@gmail.com>
2022-01-06 21:31:56 -0500

    Update plot_density_1d.py

--- dekef/plot_density_1d.py
+++ dekef/plot_density_1d.py
@@ -18,11 +18,11 @@ def plot_density_1d_params(x_limit, y_limit, plot_pts_cnt=2000, figsize=(10, 10)
         Must be of length 2. Both components must be finite numbers.
             
     y_limit : tuple
-        The tuple to specify the domain of the plot of density estimate in the vertical axis.
+        The tuple to specify the domain of the plot of the density estimate in the vertical axis.
         Must be of length 2. Both components must be finite numbers.
         
     plot_pts_cnt : int, optional
-        The number of points to be evaluated along the plot_domain to make a plot of density estimate;
+        The number of points to be evaluated along the plot_domain to make a plot of the density estimate;
         default is 2000.
         
     figsize : typle, optional
@@ -126,7 +126,7 @@ def plot_density_1d(data, kernel_function, base_density, basis_type, coef, norma
         Only need to supple when basis_type is 'grid_points'. Default is None.
         
     save_plot : bool, optional
-        Whether to save the plot of the density estimate as a local file; default is False.
+        Whether to save the plot of the density estimate to a local file; default is False.
     
     save_dir : str, optional
         The directory path to which the plot of the density estimate is saved;
@@ -139,8 +139,8 @@ def plot_density_1d(data, kernel_function, base_density, basis_type, coef, norma
     Returns
     -------
     dict
-        A dictionary of x_vals, the values of the horizontal axis for plotting, and
-        den_vals, the values of the vertical axis for plotting.
+        A dictionary of 'x_vals', the values of the horizontal axis for plotting, and
+        'den_vals', the values of the vertical axis for plotting.
     
     """
 
